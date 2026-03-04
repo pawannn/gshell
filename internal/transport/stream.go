797a7fb277764d05dc8aecf6495fe3d322f339de
pawannn <pawankalyan1614@gmail.com>
2026-03-05 00:03:21 +0530

    chore: removed fanout fun

--- internal/transport/stream.go
+++ internal/transport/stream.go
@@ -9,19 +9,3 @@ func Pipe(a io.ReadWriter, b io.ReadWriter) {
 	go io.Copy(a, b)
 	io.Copy(b, a)
 }
-
-// FanOut sends data from src to multiple writers
-func FanOut(src io.Reader, writers ...io.Writer) {
-	buf := make([]byte, 4096)
-
-	for {
-		n, err := src.Read(buf)
-		if err != nil {
-			return
-		}
-
-		for _, w := range writers {
-			w.Write(buf[:n])
-		}
-	}
-}
